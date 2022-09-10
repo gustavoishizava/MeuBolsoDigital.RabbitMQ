e0712bf821bda99b60745d546ac8a3752f8234c7
Gustavo Cabral Ishizava <gustavo@MacBook-Pro-de-Gustavo.local>
2022-09-10 17:29:13 -0300

    fix rabbit port

--- src/MeuBolsoDigital.RabbitMQ/Extensions/IServiceCollectionExtensions.cs
+++ src/MeuBolsoDigital.RabbitMQ/Extensions/IServiceCollectionExtensions.cs
@@ -8,12 +8,13 @@ namespace MeuBolsoDigital.RabbitMQ.Extensions
     {
         public static IServiceCollection AddRabbitMqConnection(this IServiceCollection services, IConfiguration configuration)
         {
+            int port = int.TryParse(configuration["RabbitMqConfiguration:Port"], out int rabbitPort) ? rabbitPort : 5672;
             services.AddSingleton<IConnectionFactory>(new ConnectionFactory
             {
                 HostName = configuration["RabbitMqConfiguration:HostName"],
                 UserName = configuration["RabbitMqConfiguration:UserName"],
                 Password = configuration["RabbitMqConfiguration:Password"],
-                Port = int.Parse(configuration["RabbitMqConfiguration:Port"])
+                Port = port
             });
 
             services.AddSingleton<IRabbitMqConnection, RabbitMqConnection>();
